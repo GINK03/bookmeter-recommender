0713383b54a4498cd865171c98b9d933307e8763
Ubuntu <ubuntu@ip-172-31-31-206.ap-northeast-1.compute.internal>
2017-11-20 02:05:51 +0000

    a

--- scrape.py
+++ scrape.py
@@ -3,7 +3,8 @@ import bs4
 import math
 import time
 import sys
-import plyvel
+#import plyvel
+import os
 
 import pickle
 import gzip
@@ -13,21 +14,24 @@ from requests.auth import HTTPProxyAuth
 
 import random
 import json
+
 def _map1(url):
   print('now scraping', url)
   try:
-    #auth = HTTPProxyAuth("irep", "irepawr003")
-    if random.random() < 0.5:
-      proxy = random.choice( proxys )
-      req = requests.get(url, proxies=proxy )
-    else:
-      req = requests.get(url )
+    headers = {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:56.0) Gecko/20100101 Firefox/56.0',
+    }
+    #if random.random() < 0.5:
+    #proxy = random.choice( proxys )
+    #req = requests.get(url, proxies=proxy )
+    #else:
+    req = requests.get(url, headers=headers )
       
     if( req.status_code != 200 ):
       print('status code', req.status_code )
       return url, None, None, None
 
-    soup = bs4.BeautifulSoup( req.text, 'html5lib' )
+    soup = bs4.BeautifulSoup( req.text, 'lxml' )
     
     _links = []
     for link in soup.find_all('a', href=True):
@@ -37,18 +41,21 @@ def _map1(url):
           href = 'https://bookmeter.com' + href
       except IndexError:
         continue
-      if 'https://bookmeter.com/users' not in href:
+      if 'https://bookmeter.com/users' not in href or 'read' not in href:
         continue
       _links.append( href )
+      print(href)
+    local_name = 'htmls/{}.pkl.gz'.format( url.replace('/', '_') )
+    open(local_name,'wb').write( gzip.compress(pickle.dumps( (req.text, _links ) )) )
     print('normaly done, ', url)
     return url, req.text, _links, None
   except Exception as e:
     print('Deep Error', e)
     # 原因となったproxyを削除する
-    proxys.remove(proxy)
+    #proxys.remove(proxy)
     return url, None, None, None
 
-db = plyvel.DB('htmls.ldb', create_if_missing=True)
+#db = plyvel.DB('htmls.ldb', create_if_missing=True)
 proxys = []
 for triple in json.loads(open('misc/proxies.json').read() ):
   ip, port, cn = triple
@@ -64,19 +71,23 @@ def scrape():
       break
     urls = links
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=512) as exe:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=1) as exe:
       for url, html, _links, soup in exe.map( _map1, urls):
         if html is None:
           continue # dbにも入れない
-        db.put(bytes(url,'utf8'), gzip.compress(pickle.dumps( (html, _links ) )))
+        #db.put(bytes(url,'utf8'), gzip.compress(pickle.dumps( (html, _links ) )))
         links.remove(url)
+        open('tmp/finished/' + url.replace('/','_'), 'a' )
         for _link in _links:
-          if db.get(bytes(_link, 'utf8')) is not None:
+          #if db.get(bytes(_link, 'utf8')) is not None:
+          #  continue
+          if os.path.exists('tmp/finished/' + url.replace('/','_')) is True:
             continue
           print('find new link', _link)
           links.append( _link )
 
-    time.sleep(0.1)
+    #time.sleep(0.1)
+    break
 
 def dump():
   links = set()
@@ -94,7 +105,7 @@ def dump():
           href = 'https://bookmeter.com' + href
       except IndexError:
         continue
-      if 'https://bookmeter.com/users' not in href:
+      if 'https://bookmeter.com/users' not in href or 'read' not in href:
         continue
       links.add( href )
   

5fa78a7625ac713a6a921894a0febfb91fc569f3
G K <gim.kobayashi@gmail.com>
2017-11-27 00:35:03 +0900

    a

--- recoomender-fasttext/freq.py
+++ recoomender-fasttext/freq.py
@@ -1,15 +1,39 @@
 import json
 import sys
-book_freq = {}
-for index, line in enumerate(open('dump.jsonp')):
-  if index%1000 == 0:
-    print('iter', index, file=sys.stderr )
-  line = line.strip()
-  flag, books = json.loads( line )
-  for book in books:
-    if book_freq.get(book) is None:
-      book_freq[book] = 0
-    book_freq[book] += 1
 
-for book, freq in sorted(book_freq.items(), key=lambda x:x[1]*-1):
-  print( book, freq )
+
+if '--normal' in sys.argv:
+  book_freq = {}
+  for index, line in enumerate(open('dump.jsonp')):
+    if index%1000 == 0:
+      print('iter', index, file=sys.stderr )
+    line = line.strip()
+    flag, books = json.loads( line )
+    for book in books:
+      if book_freq.get(book) is None:
+        book_freq[book] = 0
+      book_freq[book] += 1
+
+  for book, freq in sorted(book_freq.items(), key=lambda x:x[1]*-1):
+    print( book, freq )
+
+if '--6month' in sys.argv:
+  book_freq_0 = {}
+  book_freq_1 = {}
+  for index, line in enumerate(open('dump.jsonp')):
+    if index%1000 == 0:
+      print('iter', index, file=sys.stderr )
+    line = line.strip()
+    flag, books = json.loads( line )
+    for book in books:
+      if flag == 1:
+        if book_freq_1.get(book) is None:
+          book_freq_1[book] = 0
+        book_freq_1[book] += 1
+      if flag == 0:
+        if book_freq_0.get(book) is None:
+          book_freq_0[book] = 0
+        book_freq_0[book] += 1
+
+  for book, freq in sorted(book_freq_0.items(), key=lambda x:x[1]*-1):
+    print('0', book, freq )

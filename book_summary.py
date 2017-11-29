51992ba56abee7d2d0f0a62e35de33e7d25f83a4
G K <gim.kobayashi@gmail.com>
2017-11-30 08:10:37 +0900

    a

--- book_summary.py
+++ book_summary.py
@@ -14,29 +14,46 @@ import re
 import sys
 
 import os
-if '--poll' in sys.argv:
-  while True:
-    for name in glob.glob('htmls/*'):
-      if 'bookmeter.com_books_' in name:
-        try:
-          html, links = pickle.loads( gzip.decompress( open(name, 'rb').read() ) )
-        except:
-          continue
-        soup = bs4.BeautifulSoup(html)
-        title = soup.find('h1', {'class':'inner__title'})
-        summary = soup.find('div', {'class':'book-summary__default'}) 
-        if title is None or summary is None:
-          continue
-        title = title.text
-        if os.path.exists('summaries/{}.json'.format(title.replace('/', '_'))) is True:
-          continue
-        summary = summary.text
-        print( name)
-        print(title)
 
-        print( summary )
-        
-        try:
-          open('summaries/{}.json'.format(title.replace('/', '_')), 'w').write( json.dumps({'name':name, 'title':title, 'summary':summary}, indent=2, ensure_ascii=False))
-        except Exception:
-          continue
+import concurrent.futures 
+if '--poll' in sys.argv:
+
+  def _map1(arr):
+    index, names = arr
+
+    for name in names:
+      try:
+        html, links = pickle.loads( gzip.decompress( open(name, 'rb').read() ) )
+      except:
+        continue
+      soup = bs4.BeautifulSoup(html)
+      title = soup.find('h1', {'class':'inner__title'})
+      summary = soup.find('div', {'class':'book-summary__default'}) 
+      if title is None or summary is None:
+        continue
+      title = title.text
+      if os.path.exists('summaries/{}.json'.format(title.replace('/', '_'))) is True:
+        continue
+      summary = summary.text
+      print( name)
+      print(title)
+
+      print( summary )
+      
+      try:
+        open('summaries/{}.json'.format(title.replace('/', '_')), 'w').write( json.dumps({'name':name, 'title':title, 'summary':summary}, indent=2, ensure_ascii=False))
+      except Exception:
+        continue
+  while True:
+    arrs = {} 
+    for index, name in enumerate(glob.glob('htmls/*')):
+      if 'bookmeter.com_books_' in name:
+        key = index%8
+        if arrs.get(key) is None:
+          arrs[key] = []
+        arrs[key].append( name )
+    arrs = [ (index, names) for index, names in arrs.items()] 
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as exe:
+      exe.map(_map1, arrs)
+      
+    
